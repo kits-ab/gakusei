4d24b5080544b4316e27a8767fdedbed4c89a671
David Chang <changis2004@gmail.com>
2016-12-08 08:52:52 +0100

    individual id number generation for nugget/fact
    individual id number generation for nugget/fact

--- src/main/java/se/kits/gakusei/content/model/Fact.java
+++ src/main/java/se/kits/gakusei/content/model/Fact.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 public class Fact implements Serializable{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- src/main/java/se/kits/gakusei/content/model/Nugget.java
+++ src/main/java/se/kits/gakusei/content/model/Nugget.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Nugget implements Serializable{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
