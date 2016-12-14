dca0ca39e6e18810d9c06242f09ce53a9479af1e
Pär Svedberg <par.svedberg@gmail.com>
2016-12-14 14:37:49 +0100

    Add simple React-Bootstrap

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,37 @@
+class GakuseiNav extends React.Component {
+    render() {
+        return (
+        <ReactBootstrap.Navbar inverse collapseOnSelect>
+            <ReactBootstrap.Navbar.Header>
+                <ReactBootstrap.Navbar.Brand>
+                    <span><a href="#"><img height={'100%'} src="/img/temp_gakusei_logo3.png" alt="Gakusei logo"/></a>Gakusei</span>
+                </ReactBootstrap.Navbar.Brand>
+                <ReactBootstrap.Navbar.Toggle />
+            </ReactBootstrap.Navbar.Header>
+            <ReactBootstrap.Navbar.Collapse>
+                {/*<ReactBootstrap.Nav>*/}
+                    {/*<ReactBootstrap.NavItem eventKey={1} href="#">Link</ReactBootstrap.NavItem>*/}
+                    {/*<ReactBootstrap.NavItem eventKey={2} href="#">Link</ReactBootstrap.NavItem>*/}
+                    {/*<ReactBootstrap.NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">*/}
+                        {/*<ReactBootstrap.MenuItem eventKey={3.1}>Action</ReactBootstrap.MenuItem>*/}
+                        {/*<ReactBootstrap.MenuItem eventKey={3.2}>Another action</ReactBootstrap.MenuItem>*/}
+                        {/*<ReactBootstrap.MenuItem eventKey={3.3}>Something else here</ReactBootstrap.MenuItem>*/}
+                        {/*<ReactBootstrap.MenuItem divider />*/}
+                        {/*<ReactBootstrap.MenuItem eventKey={3.3}>Separated link</ReactBootstrap.MenuItem>*/}
+                    {/*</ReactBootstrap.NavDropdown>*/}
+                {/*</ReactBootstrap.Nav>*/}
+                <ReactBootstrap.Nav pullRight>
+                    <ReactBootstrap.NavItem eventKey={2} href="#">About</ReactBootstrap.NavItem>
+                    <ReactBootstrap.Navbar.Text>
+                        <ReactBootstrap.Navbar.Link href="/logout">Logout</ReactBootstrap.Navbar.Link>
+                    </ReactBootstrap.Navbar.Text>
+                </ReactBootstrap.Nav>
+            </ReactBootstrap.Navbar.Collapse>
+        </ReactBootstrap.Navbar>
+        )
+    }
+}
+
 class AnswerButton extends React.Component {
     onClick() {
         fetch(`http://localhost:8080/api/question/${this.props.label}`, {
@@ -9,7 +43,7 @@ class AnswerButton extends React.Component {
         })
     }
     render() {
-        return <button type="button" onClick={this.onClick.bind(this)}>{this.props.label}</button>
+        return <ReactBootstrap.Button bsSize="large" block type="button" onClick={this.onClick.bind(this)}>{this.props.label}</ReactBootstrap.Button>
     }
 }
 
@@ -36,12 +70,31 @@ class App extends React.Component {
     render() {
         return (
         <div>
-            <h1>Gakusei</h1>
-            <h2>{this.state.question}</h2>
-            <AnswerButton label = {this.state.alternative1}/>
-            <AnswerButton label = {this.state.alternative2}/>
-            <AnswerButton label = {this.state.alternative3}/>
-            <AnswerButton label = {this.state.correctAlternative}/>
+        <GakuseiNav/>
+        <ReactBootstrap.Grid>
+            <ReactBootstrap.Row><h2 className="text-center">{this.state.question}</h2></ReactBootstrap.Row>
+            <ReactBootstrap.Row>
+                <ReactBootstrap.ButtonToolbar block>
+                    <ReactBootstrap.Col xs={5} xsOffset={1} sm={4} smOffset={2} md={3} mdOffset={3}>
+                        <AnswerButton label = {this.state.alternative1}/>
+                    </ReactBootstrap.Col>
+                    <ReactBootstrap.Col xs={5} sm={4} md={3}>
+                        <AnswerButton label = {this.state.alternative2}/>
+                    </ReactBootstrap.Col>
+                </ReactBootstrap.ButtonToolbar>
+            </ReactBootstrap.Row>
+            <br/>
+            <ReactBootstrap.Row>
+                <ReactBootstrap.ButtonToolbar>
+                    <ReactBootstrap.Col xs={5} xsOffset={1} sm={4} smOffset={2} md={3} mdOffset={3}>
+                        <AnswerButton label = {this.state.alternative3}/>
+                    </ReactBootstrap.Col>
+                    <ReactBootstrap.Col xs={5} sm={4} md={3}>
+                        <AnswerButton label = {this.state.correctAlternative}/>
+                    </ReactBootstrap.Col>
+                </ReactBootstrap.ButtonToolbar>
+            </ReactBootstrap.Row>
+        </ReactBootstrap.Grid>
         </div>
         );
     }
