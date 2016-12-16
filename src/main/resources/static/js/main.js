222cc975f2c9008d64702c9787e7365fca5a9fb8
Pär Svedberg <par.svedberg@gmail.com>
2016-12-16 11:01:48 +0100

    Add navbar and other react-bootstrap lipstick

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -40,34 +40,16 @@ class AnswerButton extends React.Component {
                       answered: ""
         };
       }
-
       componentDidMount(){
             this.setState({correctAnswer: this.props.correctAnswer,
                     answered: this.props.label});
       }
-
-    onClick() {
-
-//        fetch("http://localhost:8080/api/answer", {
-//            method: 'POST',
-//            headers: {
-//                'Accept': 'application/json',
-//                'Content-Type': 'application/json',
-//            },
-//            body: JSON.stringify({'answer': this.props.label})
-//        });
-    }
     render() {
         return (
-
-            <ReactBootstrap.Button bsStyle={this.state.buttonStyle} bsSize="large" onClick={this.props.onAnswerClick.bind(this, this.props.label)}> {this.props.label} </ReactBootstrap.Button>
-
+            <ReactBootstrap.Button bsStyle={this.state.buttonStyle} bsSize="large" block onClick={this.props.onAnswerClick.bind(this, this.props.label)}> {this.props.label} </ReactBootstrap.Button>
         )
     };
 }
-
-//this.onClick.bind(this)
-//this.props.onAnswerClick
 
 class NextButton extends React.Component{
 
@@ -79,7 +61,6 @@ class NextButton extends React.Component{
                         alternative3: '',
                         correctAlternative: ''};
     }
-
     render(){
         return(
             <ReactBootstrap.Button bsStyle="info" onClick={this.props.onMagicClick}> Next Question </ReactBootstrap.Button>
@@ -88,16 +69,13 @@ class NextButton extends React.Component{
 }
 
 class ResultText extends React.Component{
-
     constructor(props) {
         super(props);
         this.state = {resultDisplay: "Click answer."};
     }
-
     componentDidMount() {
         this.setState({resultDisplay: this.props.result});
     }
-
     render(){
         return(
             <h4>{this.props.result}</h4>
@@ -121,7 +99,7 @@ class App extends React.Component {
     }
 
     fetchQuestion(){
-            fetch('http://localhost:8080/api/question/', {credentials: "same-origin"})
+            fetch('/api/question/', {credentials: "same-origin"})
                 .then(result => result.json())
                 .then(response => this.setState({question: response.question,
                 alternative1: response.alternative1,
@@ -131,15 +109,13 @@ class App extends React.Component {
                 }));
 
             this.setState({
-                answerReturn: "click the answer"
+                answerReturn: ""
             });
     }
 
     postAnswer(answer){
-        //alert('TAADAH!');
-
         this.setState({
-            answerReturn: (answer === this.state.correctAlternative) ? "correct" : "incorrect"
+            answerReturn: (answer === this.state.correctAlternative) ? "Correct!" : "Incorrect"
         });
     }
 
@@ -150,29 +126,43 @@ class App extends React.Component {
     render() {
         return (
         <div>
-            <h1>Gakusei</h1>
-            <h2>{this.state.question}</h2>
-            <form>
-                <ReactBootstrap.ButtonToolbar>
-                    <AnswerButton label = {this.state.alternative1} correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
-
-                    <AnswerButton label = {this.state.alternative2}  correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
-
-                    <AnswerButton label = {this.state.alternative3} correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
-
-                    <AnswerButton label = {this.state.correctAlternative} correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
+        <GakuseiNav/>
+        <ReactBootstrap.Grid>
+            <ReactBootstrap.Row><h2 className="text-center">{this.state.question}</h2></ReactBootstrap.Row>
+            <br/>
+            <ReactBootstrap.Row>
+                <ReactBootstrap.ButtonToolbar block>
+                    <ReactBootstrap.Col xs={5} xsOffset={1} sm={4} smOffset={2} md={3} mdOffset={3}>
+                        <AnswerButton label = {this.state.alternative1} correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
+                    </ReactBootstrap.Col>
+                    <ReactBootstrap.Col xs={5} sm={4} md={3}>
+                        <AnswerButton label = {this.state.alternative2}  correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
+                    </ReactBootstrap.Col>
                 </ReactBootstrap.ButtonToolbar>
-            </form>
-
-            <div>{this.state.answerReturn}</div>
+            </ReactBootstrap.Row>
+            <br/>
+            <ReactBootstrap.Row>
+                <ReactBootstrap.ButtonToolbar block>
+                    <ReactBootstrap.Col xs={5} xsOffset={1} sm={4} smOffset={2} md={3} mdOffset={3}>
+                        <AnswerButton label = {this.state.alternative3} correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
+                    </ReactBootstrap.Col>
+                    <ReactBootstrap.Col xs={5} sm={4} md={3}>
+                        <AnswerButton label = {this.state.correctAlternative} correctAnswer={this.state.correctAlternative} onAnswerClick={this.postAnswer} buttonStyle="default" />
+                    </ReactBootstrap.Col>
+                </ReactBootstrap.ButtonToolbar>
+            </ReactBootstrap.Row>
             <br/><br/>
-            <NextButton onMagicClick={this.fetchQuestion}/>
-
+            <ReactBootstrap.Row>
+                <div className="text-center"><NextButton onMagicClick={this.fetchQuestion}/></div>
+            </ReactBootstrap.Row>
+            <br/>
+            <ReactBootstrap.Row>
+                <div className="text-center">{this.state.answerReturn}</div>
+            </ReactBootstrap.Row>
+        </ReactBootstrap.Grid>
         </div>
         );
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// <ResultText result={this.state.answerReturn}/>
