32a3981a351c6e4af9c4e27a3a52f06a3a2ec65b
Pär Svedberg <par.svedberg@kits.se>
2017-01-10 10:32:51 +0100

    Switch ascii smileys to emoji in gameplay feedback

--- js-src/react/components/guessplaypage.js
+++ js-src/react/components/guessplaypage.js
@@ -72,7 +72,7 @@ export default class GuessPlayPage extends React.Component {
         return array;
     }
     checkAnswer(answer) {
-        var newButtonStyles = [];
+        let newButtonStyles = [];
         if (answer === this.state.correctAlt) {
             newButtonStyles = this.state.randomOrderAlt.map( (word) => (word === answer) ?
             'success' : 'default');
@@ -103,29 +103,36 @@ export default class GuessPlayPage extends React.Component {
         }
     }
     getSuccessRate(){
-        var successRate = 0;
-        var successRateMessage = '';
+        let successRate = 0;
+        let successRateMessage = '';
+        const emojiFeedback = {
+            veryBad : String.fromCodePoint(0x1F61E),
+            bad : String.fromCodePoint(0x1F615),
+            average : String.fromCodePoint(0x1F610),
+            good : String.fromCodePoint(0x1F642),
+            veryGood : String.fromCodePoint(0x1F600)
+        };
         if (Number(sessionStorage.totalAttempts) > 0) {
             successRate = Number(sessionStorage.correctAttempts)
                 / Number(sessionStorage.totalAttempts) * 100;
-            successRateMessage = successRate.toFixed(0) + '%';
+            successRateMessage = `${successRate.toFixed(0)} %`;
             if(successRate >= 80){
-                return ', ' + successRateMessage + ' :D';
+                return `, ${successRateMessage} ${emojiFeedback['veryGood']}`;
             } else if(successRate < 80 && successRate >= 60){
-                return ', ' + successRateMessage + ' :)';
+                return `, ${successRateMessage} ${emojiFeedback['good']}`;
             } else if(successRate < 60 && successRate >= 40){
-                return ', ' + successRateMessage + ' :/';
+                return `, ${successRateMessage} ${emojiFeedback['average']}`;
             } else if(successRate < 40 && successRate >= 20){
-                return ', ' + successRateMessage + ' :(';
+                return `, ${successRateMessage} ${emojiFeedback['bad']}`;
             } else if(successRate < 20){
-                return ', ' + successRateMessage + ' :((';
+                return `, ${successRateMessage} ${emojiFeedback['veryBad']}`;
             }
         } else {
               return successRateMessage;
         }
     }
     onKeys(event){
-        var keyDown = event.key;
+        let keyDown = event.key;
         if(!this.state.buttonDisabled){
             if (keyDown === '1') {
                 this.checkAnswer(this.state.randomOrderAlt[0]);
