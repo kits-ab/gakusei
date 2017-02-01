9e85382af33b449f922d4ca016b0b27d503e12c6
JoakimDanielsson <joakim.danielsson1990@gmail.com>
2017-02-01 15:36:22 +0100

    Set RestResource of events to false

--- src/main/java/se/kits/gakusei/user/repository/EventRepository.java
+++ src/main/java/se/kits/gakusei/user/repository/EventRepository.java
@@ -2,9 +2,11 @@ package se.kits.gakusei.user.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import se.kits.gakusei.user.model.Event;
 
+@RestResource(exported = false)
 public interface EventRepository extends CrudRepository<Event, Long> {
 
     @Override
