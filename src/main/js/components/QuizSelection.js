0083b5648a444b2b0b288ff518ce61cb05367345
Pär Svedberg <par.svedberg@kits.se>
2017-02-01 15:25:47 +0100

    Remove placeholder quiz names

--- src/main/js/components/QuizSelection.js
+++ src/main/js/components/QuizSelection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GenericSelection from './GenericSelection';
 
-const lessons = ['Den japanska floran', 'Japanska städer 3', 'Fujis vulkaniska bergarter'];
+const lessons = ['Den japanska floran'];
 const QuizSelection = props => <GenericSelection {...props} lessonNames={lessons} fetchURL="/api/quiz" />;
 
 export default QuizSelection;
