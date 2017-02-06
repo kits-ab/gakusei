81098e3a16b3454964c623ce071f3bd421d2ce03
William Bernting <william.bernting@kits.se>
2017-02-06 10:53:47 +0100

    eslint fix (no-param-reassign)

--- src/main/js/util/Utility.js
+++ src/main/js/util/Utility.js
@@ -55,12 +55,13 @@ export default class Utility {
   }
 
   static randomizeOrder(array) {
-    for (let i = array.length - 1; i > 0; i -= 1) {
+    const newArray = array.slice();
+    for (let i = newArray.length - 1; i > 0; i -= 1) {
       const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+      const temp = newArray[i];
+      newArray[i] = newArray[j];
+      newArray[j] = temp;
     }
-    return array;
+    return newArray;
   }
 }
