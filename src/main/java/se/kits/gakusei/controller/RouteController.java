33cf80f0f412ac86f20f299d41f695db71556667
William Bernting <william.bernting@kits.se>
2017-03-02 11:12:09 +0100

    RouteController: URL "whitelisting" to front-end

---
+++ src/main/java/se/kits/gakusei/controller/RouteController.java
@@ -0,0 +1,40 @@
+package se.kits.gakusei.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import se.kits.gakusei.content.model.Lesson;
+import se.kits.gakusei.content.repository.LessonRepository;
+import se.kits.gakusei.util.QuestionHandler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RouteController {
+    @GetMapping("/{path:(?!.*.js|.*.css|.*.jpg).*$}") // To not disrupt any resources
+    @RequestMapping(value = {
+            "/",
+            "/login",
+            "/logout",
+            "/play/**",
+            "/select/**",
+            "/translate",
+            "/finish/**",
+            "/profile",
+            "/about"},
+            method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
