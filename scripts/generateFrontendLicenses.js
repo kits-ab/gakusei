56cb0a12eab8d397f5823c4a3af496d707f4e85c
William Bernting <william.bernting@kits.se>
2017-04-03 09:40:28 +0200

    bugfixes

--- scripts/generateFrontendLicenses.js
+++ scripts/generateFrontendLicenses.js
@@ -7,11 +7,14 @@ const fs = require('fs');
 const timerMessage = 'package.json dependencies written to frontend_licenses.json';
 const timerMessageDev = 'dummy package.json dependencies written to frontend_licenses.json';
 
+const licenseDirectory = path.resolve('./src/main/resources/static/license/');
+const licenseFilePath = path.resolve(licenseDirectory, 'frontend_licenses.json');
+
 if (process.env.NODE_ENV === 'production') {
   console.time(timerMessage);
   checker.init({
     start: '.',
-    json: path.resolve('../frontend_licenses.json')
+    json: true
   }, (err, json) => {
     if (err) {
       throw err;
@@ -24,10 +27,12 @@ if (process.env.NODE_ENV === 'production') {
       });
 
       //   console.log(newJson);
-      fs.mkdirSync(path.resolve('./src/main/resources/static/license/'));
-      fs.writeFile(path.resolve('./src/main/resources/static/license/frontend_licenses.json'), JSON.stringify(newJson), (err2) => {
-        if (err2) {
-          throw err2;
+      try {
+        fs.mkdirSync(licenseDirectory);
+      } catch (writeErr) { /* File likely already exists */ }
+      fs.writeFile(licenseFilePath, JSON.stringify(newJson), (writeErr) => {
+        if (writeErr) {
+          throw writeErr;
         }
       });
       console.timeEnd(timerMessage);
@@ -41,9 +46,12 @@ if (process.env.NODE_ENV === 'production') {
     repository: 'http://github.com/dummypackage'
   } };
 
-  fs.writeFile(path.resolve('./src/main/resources/static/license/frontend_licenses.json'), JSON.stringify(dummyPackage), (err2) => {
-    if (err2) {
-      throw err2;
+  try {
+    fs.mkdirSync(licenseDirectory);
+  } catch (writeErr) { /* File likely already exists */ }
+  fs.writeFile(licenseFilePath, JSON.stringify(dummyPackage), (writeErr) => {
+    if (writeErr) {
+      throw writeErr;
     }
   });
 
