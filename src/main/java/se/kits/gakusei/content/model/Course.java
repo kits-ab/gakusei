67ea5d00bfe50d7abe9326654163100b0c024e0a
Meddan <christoffer.medin@gmail.com>
2017-08-31 09:40:49 +0200

    First version of Course.

--- src/main/java/se/kits/gakusei/content/model/Course.java
+++ src/main/java/se/kits/gakusei/content/model/Course.java
@@ -1,4 +1,84 @@
 package se.kits.gakusei.content.model;
 
-public class Course {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "courses", schema = "contentschema")
+public class Course implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToOne
+    private Course parent;
+
+    @ManyToMany
+    private List<Course> prerequisites;
+
+    private String description;
+
+    private int order;
+
+    @Column(nullable = false, unique = true)
+    private String courseCode;
+
+    public Course(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Course getParent() {
+        return parent;
+    }
+
+    public void setParent(Course parent) {
+        this.parent = parent;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public List<Course> getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(List<Course> prerequisites) {
+        this.prerequisites = prerequisites;
+    }
 }
