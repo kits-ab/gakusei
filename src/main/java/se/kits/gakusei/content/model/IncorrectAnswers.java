e27f2f8668e21cd2c1d2c109d8059fb454dd1449
Fredrik Öller <oller.fredrik@gmail.com>
2017-08-31 14:45:40 +0200

    Added entities: Quiz, QuizNugget and IncorrectAnswers

---
+++ src/main/java/se/kits/gakusei/content/model/IncorrectAnswers.java
@@ -0,0 +1,47 @@
+package se.kits.gakusei.content.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "incorrect_answers", schema = "contentschema")
+public class IncorrectAnswers implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String incorrectAnswer;
+
+    @ManyToOne
+    @JoinColumn(name="quiz_nugget_ref")
+    private QuizNugget quizNugget;
+
+    public IncorrectAnswers() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIncorrectAnswer() {
+        return incorrectAnswer;
+    }
+
+    public void setIncorrectAnswer(String incorrectAnswer) {
+        this.incorrectAnswer = incorrectAnswer;
+    }
+
+    public QuizNugget getQuizNugget() {
+        return quizNugget;
+    }
+
+    public void setQuizNugget(QuizNugget quizNugget) {
+        this.quizNugget = quizNugget;
+    }
+}
