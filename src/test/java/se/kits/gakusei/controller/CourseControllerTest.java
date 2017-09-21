0f203364bbaa8dc4ebfab839147b4a627e405a73
Doverstav <pontus.doverstav@kits.se>
2017-09-21 10:44:50 +0200

    Add test class for CourseController

---
+++ src/test/java/se/kits/gakusei/controller/CourseControllerTest.java
@@ -0,0 +1,47 @@
+package se.kits.gakusei.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import se.kits.gakusei.content.model.Course;
+import se.kits.gakusei.content.repository.CourseRepository;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CourseControllerTest {
+
+    @InjectMocks
+    private CourseController courseController;
+
+    @Mock
+    private CourseRepository courseRepository;
+
+    private Course testCourse;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getAllCourses() throws Exception {
+    }
+
+    @Test
+    public void getCourseByID() throws Exception {
+    }
+
+    @Test
+    public void getCourseByName() throws Exception {
+    }
+
+    @Test
+    public void getCourseByCode() throws Exception {
+    }
+
+}
