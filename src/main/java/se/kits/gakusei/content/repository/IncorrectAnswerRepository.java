5e6c7608592de99222492ad230af175d55568bc7
Sofia Edström <sofia.edstrom@kits.se>
2017-10-26 09:23:14 +0200

    remove custom exists query

--- src/main/java/se/kits/gakusei/content/repository/IncorrectAnswerRepository.java
+++ src/main/java/se/kits/gakusei/content/repository/IncorrectAnswerRepository.java
@@ -1,8 +1,6 @@
 package se.kits.gakusei.content.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import se.kits.gakusei.content.model.IncorrectAnswers;
 
 import java.util.List;
@@ -11,6 +9,4 @@ public interface IncorrectAnswerRepository extends CrudRepository<IncorrectAnswe
 
     List<IncorrectAnswers> getByQuizNugget_Id(Long id);
 
-    @Query("SELECT COUNT(e)>0 from IncorrectAnswers e WHERE e.id = :id AND e.quizNugget.id = :quizNuggetId")
-    boolean existsByIdAndQuizNuggetId(@Param("id") Long id, @Param("quizNuggetId") Long quizNuggetId);
 }
