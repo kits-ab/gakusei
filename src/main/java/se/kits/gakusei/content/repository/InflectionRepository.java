a94c2abb92d3056dc5c7716df86f12ea0e8b59c2
Doverstav <pontus.doverstav@kits.se>
2017-11-08 15:07:44 +0100

    Extend functionality of InflectionRepository

--- src/main/java/se/kits/gakusei/content/repository/InflectionRepository.java
+++ src/main/java/se/kits/gakusei/content/repository/InflectionRepository.java
@@ -2,6 +2,14 @@ package se.kits.gakusei.content.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import se.kits.gakusei.content.model.Inflection;
+import se.kits.gakusei.content.model.Lesson;
+
+import java.util.List;
 
 public interface InflectionRepository extends CrudRepository<Inflection, Long> {
+
+    List<Inflection> findByLesson_Id(Long id);
+
+    Inflection findByLessonAndInflectionMethod(Lesson lesson, String inflectionMethod);
+
 }
