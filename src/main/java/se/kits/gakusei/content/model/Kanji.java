99bb04b4b940673ae3a2a4dd053f52d4e7968502
Doverstav <pontus.doverstav@kits.se>
2017-11-20 11:42:09 +0100

    Initial version of kanji class

---
+++ src/main/java/se/kits/gakusei/content/model/Kanji.java
@@ -0,0 +1,81 @@
+package se.kits.gakusei.content.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "kanjis", schema = "contentschema")
+public class Kanji {
+
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+    private String description;
+
+    private boolean hidden = false;
+
+    @ManyToOne
+    @JsonBackReference(value = "kanji_book")
+    @JoinColumn(name = "book_ref")
+    private Book title;
+
+    private String swedish;
+
+    private String english;
+
+    private String kanji;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public Book getTitle() {
+        return title;
+    }
+
+    public void setTitle(Book title) {
+        this.title = title;
+    }
+
+    public String getSwedish() {
+        return swedish;
+    }
+
+    public void setSwedish(String swedish) {
+        this.swedish = swedish;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getKanji() {
+        return kanji;
+    }
+
+    public void setKanji(String kanji) {
+        this.kanji = kanji;
+    }
+}
