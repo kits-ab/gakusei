527ebb5d506900e42386c70e652644b44d7961d2
Sofia Edström <sofia.edstrom@kits.se>
2017-12-05 08:40:36 +0100

    kanji questions controller and handler

---
+++ src/main/java/se/kits/gakusei/util/KanjiHandler.java
@@ -0,0 +1,46 @@
+package se.kits.gakusei.util;
+
+import org.springframework.stereotype.Component;
+import se.kits.gakusei.content.model.Kanji;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class KanjiHandler {
+
+    public List<Kanji> chooseKanjis(List<Kanji> kanjis, int quantity) {
+        List<Kanji> visibleKanjis = kanjis.stream().filter(kanji -> !kanji.isHidden()).collect(Collectors.toList());
+        Collections.shuffle(visibleKanjis);
+
+        if (visibleKanjis.size() <= quantity) {
+            return visibleKanjis;
+        }
+
+        return visibleKanjis.subList(0, quantity);
+    }
+
+    public List<HashMap<String, Object>> createKanjiQuestions(List<Kanji> chosenKanjis) {
+        return chosenKanjis.stream().map(KanjiHandler::createKanjiQuestion).collect(Collectors.toList());
+    }
+
+    private static HashMap<String, Object> createKanjiQuestion(Kanji kanji) {
+        HashMap<String, Object> questionMap = new HashMap<>();
+
+        List<String> question = new ArrayList<>();
+        question.add(kanji.getSwedish());
+        question.add(kanji.getKanji());
+
+        questionMap.put("question", question);
+        questionMap.put("correctAlternative", Collections.singletonList(kanji.getKanji()));
+        questionMap.put("alternative1", Collections.EMPTY_LIST);
+        questionMap.put("alternative2", Collections.EMPTY_LIST);
+        questionMap.put("alternative3", Collections.EMPTY_LIST);
+        questionMap.put("questionNuggetId", kanji.getId());
+
+        return questionMap;
+    }
+}
