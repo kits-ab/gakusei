0c04f830cf2b4aa5ba85712b78fc9dcea94b9ab0
Doverstav <pontus.doverstav@kits.se>
2017-12-05 10:48:51 +0100

    Add method for finding grammarText using inflectionMethod

--- src/main/java/se/kits/gakusei/content/repository/GrammarTextRepository.java
+++ src/main/java/se/kits/gakusei/content/repository/GrammarTextRepository.java
@@ -3,5 +3,9 @@ package se.kits.gakusei.content.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.kits.gakusei.content.model.GrammarText;
 
+import java.util.List;
+
 public interface GrammarTextRepository extends CrudRepository<GrammarText, String> {
+
+    List<GrammarText> findByInflectionMethod(String inflectionMethod);
 }
