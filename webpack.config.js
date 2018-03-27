88e199747fab978077363276cf8fcd05f8bcee4f
William Bernting <william.bernting@kits.se>
2018-03-27 14:36:00 +0200

    add postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = function() {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'postcss-loader'],
           exclude
         }
       ]
