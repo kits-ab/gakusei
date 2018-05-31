d033486450b66e2ea165077bb3c7862dd0e90c7f
Anton Myrholm <anton.myrholm@kits.se>
2018-05-31 09:11:30 +0200

    Updated cache config

--- src/main/java/se/kits/gakusei/config/MvcConfigurer.java
+++ src/main/java/se/kits/gakusei/config/MvcConfigurer.java
@@ -16,13 +16,16 @@ public class MvcConfigurer
     @Value("${spring.caching:false}")
     private boolean caching;
 
+    @Value("${spring.caching-ttl:3600}")
+    private int ttl;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         CacheControl cacheControl;
         if (caching) {
             // Caching enabled via spring profile in application.yml
             cacheControl = CacheControl.maxAge(
-                3600,
+                ttl,
                 TimeUnit.SECONDS
             ).cachePublic();
         } else {
