96cf30878ef2cc51d5111eeafad30264f4a11815
Anton Myrholm <anton.myrholm@kits.se>
2018-06-07 14:38:28 +0200

    Removed unused log

--- src/main/js/shared/util/Utility.js
+++ src/main/js/shared/util/Utility.js
@@ -116,7 +116,6 @@ export default class Utility {
   }
 
   static logEvents(logData, sendImmediately) {
-    console.log(logData);
     for (let i = 0; i < logData.data.length; i++) {
       this.logEvent(
         logData.page,
