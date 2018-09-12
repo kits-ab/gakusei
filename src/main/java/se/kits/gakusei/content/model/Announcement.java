41e82ddbd0233fc353ee30eca867305e30afecd8
Daniel Ivestig <divestig@localhost.localdomain>
2018-09-12 12:38:20 +0200

    Controller work

--- src/main/java/se/kits/gakusei/content/model/Announcement.java
+++ src/main/java/se/kits/gakusei/content/model/Announcement.java
@@ -1,19 +1,54 @@
 package se.kits.gakusei.content.model;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+
 
 import javax.persistence.*;
 
+@Entity
+@Table(name = "announcements", schema = "public")
 public class Announcement implements Serializable {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Timestamp startDate;
+
+    @Column(nullable = false)
+    private Timestamp endDate;
+
+    @Column
     private String text;
 
-    public Announcement() {}
+    public Announcement() {};
 
-    public Announcement(String text) {
-        this.text = text;
+    public Long getId() {
+        return id;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
     }
 
     public String getText() {
         return text;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
--- src/main/java/se/kits/gakusei/controller/AnnouncementController.java
+++ src/main/java/se/kits/gakusei/controller/AnnouncementController.java
@@ -4,27 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import se.kits.gakusei.content.model.Announcement;
+import se.kits.gakusei.content.repository.AnnouncementRepository;
+
+import java.time.LocalDateTime;
+
 
 @RestController
-public class InfoController {
-    //@Autowired
-    //infoRepository infoRepository;
+public class AnnouncementController {
+    @Autowired
+    AnnouncementRepository announcementRepository;
 
     @RequestMapping(
             value = "api/announcement",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
-    public ResponseEntity<Announcement> getAnnouncement() {
-        Announcement a = new Announcement("Shiet");
-        if (a != null) {
-            return new ResponseEntity<>(a, HttpStatus.OK);
+    public ResponseEntity<Iterable<Announcement>> getAnnouncement() {
+        Iterable<Announcement> actualAnnouncements = announcementRepository
+                .findAnnouncementByStartDateIsBeforeAndEndDateIsAfter(LocalDateTime.now());
+        if (actualAnnouncements != null) {
+            return new ResponseEntity<>(actualAnnouncements, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
