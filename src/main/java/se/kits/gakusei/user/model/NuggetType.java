5195c16d9c2a4dd1ff7b4fcb488fea81b0efde27
Daniel Ivestig <exodius666@hotmail.com>
2018-10-02 13:36:05 +0200

    Mockito be mocking me.

--- src/main/java/se/kits/gakusei/user/model/NuggetType.java
+++ src/main/java/se/kits/gakusei/user/model/NuggetType.java
@@ -20,6 +20,10 @@ public class NuggetType implements Serializable {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getType() {
         return type;
     }
