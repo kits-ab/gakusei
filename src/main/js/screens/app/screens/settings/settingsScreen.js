0222d971bc7dbfb350599b4cba610140fddb72bb
Akar Khatab <Akar.Khatab@gmail.com>
2018-10-19 11:56:33 +0200

    Added dropdown button to settings page.

--- src/main/js/screens/app/screens/settings/settingsScreen.js
+++ src/main/js/screens/app/screens/settings/settingsScreen.js
@@ -1,33 +1,51 @@
 import * as Security from '../../../../shared/reducers/Security';
 import Utility from '../../../../shared/util/Utility';
 //import Panel from "react-bootstrap/es/Panel";
-import { Col, Grid, Panel, Row } from 'react-bootstrap';
+import { Col, DropdownButton, Grid, MenuItem, Panel, Row } from 'react-bootstrap';
 
 export const Reducers = [Security];
 
 export class settingsScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.state.title = 'Från';
+    this.changeTitle = this.changeTitle().bind(this);
   }
+  changeTitle(string) {
+    title.setState(string);
+  }
+
+  fromLangButton() {
+    return (
+      <DropdownButton title={this.props.title}>
+        <MenuItem onSelect={this.changeTitle('Svenska')}>Svenska</MenuItem>
+        <MenuItem>Japanska</MenuItem>
+        <MenuItem>Engelska</MenuItem>
+      </DropdownButton>
+    );
+  }
+  toLangButton() {
+    return (
+      <DropdownButton title="Till">
+        <MenuItem href="#books">Svenska</MenuItem>
+        <MenuItem href="#podcasts">Japanska</MenuItem>
+        <MenuItem href="#">Engelska</MenuItem>
+      </DropdownButton>
+    );
+  }
+
   render() {
     return (
       <div>
         <Grid>
-          <Row>
-            <Col xs={12}>
-              <div>
-                <Panel>
-                  <Panel.Body>
-                    <div className={'exercise'}>
-                      <div className={'exercise__header'}>
-                        <p>Testing</p>
-                      </div>
-                    </div>
-                  </Panel.Body>
-                </Panel>
-              </div>
-            </Col>
-          </Row>
+          <Col>
+            <h1>Inställningar</h1>
+            <h3>Språkalternativ</h3>
+            <div>
+              {this.fromLangButton()}
+              {this.toLangButton()}
+            </div>
+          </Col>
         </Grid>
       </div>
     );
