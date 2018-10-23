cd1ce0dff247b18b67ad07d45cf3b8de20f65231
Emma Stålesjö <emma.stalesjo@kits.se>
2018-10-23 12:24:43 +0200

    created branch for lang

---
+++ src/main/js/shared/i18n.js
@@ -0,0 +1,51 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n.use(LanguageDetector).init({
+  // we init with resources
+  resources: {
+    se: {
+      translations: {
+        'Bli student och lär dig japanska!': 'Bli student och lär dig japanska!',
+        'Läs mer': 'Läs mer',
+        'Gakusei erbjuder många funktioner som underlättar ditt lärande':
+          'Gakusei erbjuder många funktioner som underlättar ditt lärande',
+        'Testa redan nu!': 'Testa redan nu!',
+        'Registrera dig snabbt och enkelt här': 'Registrera dig snabbt och enkelt här',
+        'Gissa ordet': 'Gissa ordet',
+        Gakusei: 'Gakusei'
+      }
+    },
+    jp: {
+      translations: {
+        'Bli student och lär dig japanska!': 'スウェーデン語を学ぶ学生になる!',
+        'Läs mer': 'もっと読む',
+        'Gakusei erbjuder många funktioner som underlättar ditt lärande':
+          'Gakuseiはあなたの学習を促進する多くの機能を提供しています',
+        'Testa redan nu!': '今すぐ試してみてください！',
+        'Registrera dig snabbt och enkelt här': 'ここにすばやく簡単に登録する',
+        'Gissa ordet': '単語を推測する',
+        Gakusei: 'test'
+      }
+    }
+  },
+  fallbackLng: 'se',
+  debug: true,
+
+  // have a common namespace used around the full app
+  ns: ['translations'],
+  defaultNS: 'translations',
+
+  keySeparator: false, // we use content as keys
+
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+    formatSeparator: ','
+  },
+
+  react: {
+    wait: true
+  }
+});
+
+export default i18n;
