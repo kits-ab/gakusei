301118f7247869d564efa18704c9a4b222232ff0
Akar Khatab <Akar.Khatab@gmail.com>
2018-12-03 11:46:24 +0100

    Added the kanji client to doc/examples/.

---
+++ doc/examples/gakusei-kanji-client/documentation/member-search-index.js
@@ -0,0 +1,81 @@
+memberSearchIndex = [
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'abort()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'abortAvailability()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'answer(String)', url: 'answer-java.lang.String-' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'answerAvailability()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'getAllLessons()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'getAllLessonsAvailability()' },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'getData()' },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'getDifficulty()' },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'getNuggetid()' },
+  {
+    p: 'se.kits.gakusei.kanjiclient.service',
+    c: 'KanjiService',
+    l: 'getQuestion(String)',
+    url: 'getQuestion-java.lang.String-'
+  },
+  {
+    p: 'se.kits.gakusei.kanjiclient.service',
+    c: 'ShellService',
+    l: 'getQuestion(String)',
+    url: 'getQuestion-java.lang.String-'
+  },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'getQuestionAvailability()' },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'getTimestamp()' },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'getUsername()' },
+  {
+    p: 'se.kits.gakusei.kanjiclient.service',
+    c: 'KanjiService',
+    l: 'isLesson(String)',
+    url: 'isLesson-java.lang.String-'
+  },
+  { p: 'se.kits.gakusei.kanjiclient', c: 'KanjiClientApplication', l: 'KanjiClientApplication()' },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'KanjiDrawingDTO()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'KanjiService', l: 'KanjiService()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'KanjiService', l: 'lessons' },
+  {
+    p: 'se.kits.gakusei.kanjiclient',
+    c: 'KanjiClientApplication',
+    l: 'main(String[])',
+    url: 'main-java.lang.String:A-'
+  },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'KanjiService', l: 'printLessons()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'KanjiService', l: 'printSVG()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'printSVG()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'printSVGAvailability()' },
+  {
+    p: 'se.kits.gakusei.kanjiclient.service',
+    c: 'KanjiService',
+    l: 'sendAnswer(String)',
+    url: 'sendAnswer-java.lang.String-'
+  },
+  {
+    p: 'se.kits.gakusei.kanjiclient.dto',
+    c: 'KanjiDrawingDTO',
+    l: 'setData(String)',
+    url: 'setData-java.lang.String-'
+  },
+  {
+    p: 'se.kits.gakusei.kanjiclient.dto',
+    c: 'KanjiDrawingDTO',
+    l: 'setDifficulty(String)',
+    url: 'setDifficulty-java.lang.String-'
+  },
+  {
+    p: 'se.kits.gakusei.kanjiclient.dto',
+    c: 'KanjiDrawingDTO',
+    l: 'setNuggetid(String)',
+    url: 'setNuggetid-java.lang.String-'
+  },
+  { p: 'se.kits.gakusei.kanjiclient.dto', c: 'KanjiDrawingDTO', l: 'setTimestamp(long)' },
+  {
+    p: 'se.kits.gakusei.kanjiclient.dto',
+    c: 'KanjiDrawingDTO',
+    l: 'setUsername(String)',
+    url: 'setUsername-java.lang.String-'
+  },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'ShellService()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'KanjiService', l: 'showSVG()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'showSVG()' },
+  { p: 'se.kits.gakusei.kanjiclient.service', c: 'ShellService', l: 'showSVGAvailability()' }
+];
---
+++ doc/examples/gakusei-kanji-client/src/main/java/se/kits/gakusei/kanjiclient/KanjiClientApplication.java
@@ -0,0 +1,12 @@
+package se.kits.gakusei.kanjiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanjiClientApplication.class, args);
+    }
+}
