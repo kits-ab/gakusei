8bcc3b6e0e0d39298c7ecaa766524d399ebc1e87
oolars <lars@lornell.se>
2019-01-31 11:03:53 +0100

    kanji now should display reading column if not empty

--- src/main/java/se/kits/gakusei/util/KanjiHandler.java
+++ src/main/java/se/kits/gakusei/util/KanjiHandler.java
@@ -38,6 +38,7 @@ public class KanjiHandler {
         List<String> question = new ArrayList<>();
         question.add(kanji.getSwedish());
         question.add(kanji.getKanji());
+        question.add(kanji.getReading());
 
         questionMap.put("question", question);
         questionMap.put(
