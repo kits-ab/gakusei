996bf80e2cc837a8d8d3d1c0d2de674c217ea4fa
oolars <lars@lornell.se>
2019-02-01 09:34:53 +0100

    never get reading if null

--- src/main/java/se/kits/gakusei/util/KanjiHandler.java
+++ src/main/java/se/kits/gakusei/util/KanjiHandler.java
@@ -38,7 +38,10 @@ public class KanjiHandler {
         List<String> question = new ArrayList<>();
         question.add(kanji.getSwedish());
         question.add(kanji.getKanji());
-        question.add(kanji.getReading());
+
+        if(kanji.getReading() != null){ question.add(kanji.getReading());}
+
+
 
         questionMap.put("question", question);
         questionMap.put(
